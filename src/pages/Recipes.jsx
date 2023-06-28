1f438707a8293c359653129630064b2033512b31
ericz02 <ezheng0720@gmail.com>
2023-06-28 15:02:56 -0400

    fixes searchbar & add recipe button not rendering

--- src/pages/Recipes.jsx
+++ src/pages/Recipes.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import recipes from "../Recipes";
 import Modal from "../components/Modal";
 import RecipeModal from "../components/RecipeModal";
@@ -29,8 +29,27 @@ const Recipes = () => {
 
   return (
     <div className="mx-auto my-8 min-h-screen">
-      <div>
-        {/* ... */}
+      <h1 className="text-4xl font-bold text-center mb-4 text-gray-800">Welcome to Recipe Finder</h1>
+      <p className="text-xl text-center">Find and discover delicious recipes for every occasion.</p>
+
+      <div className="mt-8">
+        <div className="flex justify-between items-center mb-4">
+          <>
+            <Modal />
+            <div className="mr-4">
+              <input
+                type="text"
+                placeholder="Search recipes"
+                value={searchTerm}
+                onChange={handleInputChange}
+                className="px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 w-full md:w-96"
+              />
+            </div>
+          </>
+        </div>
+
+        <h2 className="text-2xl font-bold mb-4 ml-5 text-gray-800">Featured Recipes</h2>
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
           {filteredRecipes.map((recipe, index) => (
             <div
@@ -49,9 +68,11 @@ const Recipes = () => {
           ))}
         </div>
       </div>
+
       {selectedRecipe && (
         <RecipeModal recipe={selectedRecipe} onClose={handleCloseModal} />
       )}
+
     </div>
   );
 };
